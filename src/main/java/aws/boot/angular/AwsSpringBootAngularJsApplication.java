581067cf7f8948e9a4b2a79de6b9adebb5360348
Krishna Bahadur Bhat <krishna1bhat@Krishnas-MacBook-Pro.local>
2017-04-11 16:31:26 -0500

    initial commit

---
+++ src/main/java/aws/boot/angular/AwsSpringBootAngularJsApplication.java
@@ -0,0 +1,12 @@
+package aws.boot.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringBootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringBootAngularJsApplication.class, args);
+	}
+}
---
+++ src/test/java/aws/boot/angular/AwsSpringBootAngularJsApplicationTests.java
@@ -0,0 +1,16 @@
+package aws.boot.angular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsSpringBootAngularJsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
